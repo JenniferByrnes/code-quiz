30da76e6885ca532eeaca2b679cb626ded4443f5
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-06-19 11:57:41 -0400

    working on disply of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
-var questionCounter = 0;
 
+
+// Not sure that these are needed....
 var formEl = document.querySelector("#quiz-form");
 var quizQuestionEl = document.querySelector("#quiz-question");
+
+// This CAN HOLD ANSWER CHOICES!!!
 var pageContentEl = document.querySelector("#page-content");
 
-var startButton = document.getElementById("start-quiz");
+// This CAN HOLD ANSWER CHOICES!!!
+var answerChoicesEl = document.querySelector("#answer-choices");
+var i=0;
 
+// Good variables from here.
+
+var startButton = document.getElementById("start-quiz-btn");
+var choices = document.getElementById("choices");
+
+// OnClick to startQuiz function - Hide button and get questions
 function startQuiz() {
   console.log("starting quiz");
 
@@ -13,5 +24,28 @@ function startQuiz() {
   var quizStartEl = document.getElementById("start-quiz-id");
 
   quizStartEl.setAttribute("class", "hide");
-}
+  displayQuestions();
+} // end startButton
+
 startButton.onclick=startQuiz;
+
+// This uses a forEach method to cycle through answer choices
+function displayQuestions () {
+  var currentQuestion =  questions[i]
+  console.log("in displayQuestions");
+  console.log(currentQuestion);
+  console.log("starting foreach loop");
+
+  // choiceValues - doesn't seem to matter - what it is?
+  currentQuestion.choices.forEach(function(choiceValues, choiceIndex){
+ 
+    var choiceAnswer = document.createElement("button");
+    choiceAnswer.setAttribute("class", "choiceIndex");
+    choiceAnswer.setAttribute("value", choiceIndex);
+    choiceAnswer.textContent= i + 1 + choiceIndex;
+
+    console.log("choiceAnswer=",choiceAnswer);
+
+    answerChoicesEl.appendChild(choiceAnswer);
+  });
+}
